3c8a590b0648f7e4e9631c524706672405aa4b7e
tunogya <tunogya@qq.com>
2022-04-01 15:43:27 +0800

    add some command: sendMessage, sendVideo, sendPhoto

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
 #! /usr/bin/env node
-console.info('telpush:', '1.0.0')
+const { program } = require('commander');
+
+// name, description, version
+program
+    .name('telebot')
+    .description('A program for telegram bot.')
+    .version('1.0.0')
+
+// options
+program
+    .option('-m --mode [mode]', 'send message mode, mess or single')
+    .option('-t, --token [token]', `telegram bot's token`)
+
+
+// command: sendMessage
+program
+    .command('sendMessage <text> [chat_id]')
+    .description('send message via telegram bot')
+    .action((text, chat_id) => {
+        const options = program.opts();
+        console.log(`bot token: ${options.token}`)
+        console.log(`sendMessage: ${text}`)
+        console.log(`to user: ${chat_id}`)
+    })
+
+program
+    .command('sendVideo <video> [chat_id]')
+    .description('send video via telegram bot')
+    .action((video, chat_id) => {
+        const options = program.opts();
+        console.log(`bot token: ${options.token}`)
+        console.log(`sendVideo: ${video}`)
+        console.log(`to user: ${chat_id}`)
+    })
+
+program
+    .command('sendPhoto <photo> [chat_id]')
+    .description('send photo via telegram bot')
+    .action((photo, chat_id) => {
+        const options = program.opts();
+        console.log(`bot token: ${options.token}`)
+        console.log(`sendVideo: ${photo}`)
+        console.log(`to user: ${chat_id}`)
+    })
+
+program
+    .parse()
+
+
+
