1d8d7c9eb8d9366e875d6c48f79a255b7de95bd1
tunogya <tunogya@qq.com>
2022-04-01 16:33:57 +0800

    update

--- README.md
+++ README.md
@@ -28,51 +28,16 @@ get version of telebot.
 telebot -V
 ```
 
-## sendMessage
+## send
 
 only send a message to single chat_id.
 
 ```shell
-telebot -t <token> sendMessage <text> chat_id
+telebot -T <token> sendMessage -t <text> [chat_id]
 ```
 
 if you want to send message to chat_ids which save in a csv file.
 
 ```shell
-telebot -t <token> sendMessage <text> -m mess -f /path/file.csv
+telebot -T <token> sendMessage -t <text> -m -f /path/file.csv
 ```
-
-
-## sendVideo
-
-only send a video to single chat_id.
-
-```shell
-telebot -t <token> sendVideo <video> chat_id -c [caption]
-```
-
-if you want to send video to chat_ids which save in a csv file.
-
-```shell
-telebot -t <token> sendVideo <video> -m mess -f /path/file.csv -c [caption]
-```
-
-PS: `video` can be a URI or telegram file id or file path of local.
-
-
-## sendPhoto
-
-only send a photo to single chat_id.
-
-```shell
-telebot -t <token> sendPhoto <photo> chat_id -c [caption]
-```
-
-if you want to send video to chat_ids which save in a csv file.
-
-```shell
-telebot -t <token> sendPhoto <photo> -m mess -f /path/file.csv -c [caption]
-```
-
-PS: `photo` can be a URI or telegram file id or file path of local.
-
--- src/index.js
+++ src/index.js
@@ -9,43 +9,28 @@ program
 
 // options
 program
-    .option('-m --mode [mode]', 'send message mode, mess or single', 'single')
-    .option('-t, --token [token]', `telegram bot's token`)
+    .option('-T, --token <token>', `telegram bot's token`)
+    .option('-f, --file <file>', 'file path')
+    .option('-t, --text <text>', 'text')
+    .option('-c, --text <caption>', 'caption')
+    .option('-p, --photo <photo>', 'photo uri or photo id')
+    .option('-v, --video <video>', 'video uri or video id')
+    .option('-m, --mess', 'mess send mode')
 
-
-// command: sendMessage
+// command: send
 program
-    .command('sendMessage <text> [chat_id]')
-    .description('send message via telegram bot')
-    .action((text, chat_id) => {
+    .command('send [chat_id]')
+    .description('send message or photo or video')
+    .action((chat_id) => {
         const options = program.opts();
-        console.log(`bot token: ${options.token}`)
-        console.log(`send message: ${text}`)
+        console.log(`bot: ${options.token}`)
+        console.log(`send message: ${options.text}`)
+        console.log(`send video: ${options.video}`)
+        console.log(`send photo: ${options.photo}`)
         console.log(`send mode: ${options.mode}`)
         console.log(`to user: ${chat_id}`)
     })
 
-program
-    .command('sendVideo <video> [chat_id]')
-    .description('send video via telegram bot')
-    .action((video, chat_id) => {
-        const options = program.opts();
-        console.log(`bot token: ${options.token}`)
-        console.log(`send video: ${video}`)
-        console.log(`send mode: ${options.mode}`)
-        console.log(`to user: ${chat_id}`)
-    })
-
-program
-    .command('sendPhoto <photo> [chat_id]')
-    .description('send photo via telegram bot')
-    .action((photo, chat_id) => {
-        const options = program.opts();
-        console.log(`bot token: ${options.token}`)
-        console.log(`send video: ${photo}`)
-        console.log(`send mode: ${options.mode}`)
-        console.log(`to user: ${chat_id}`)
-    })
 
 program
     .parse()
